6c620fed8dcd5cc411f4462cb954962baab42a4c
Valentina Dibs <tdibs@truecar.com>
2017-08-29 23:23:47 -0700

    remove node_modules, start fixes

--- src/App.js
+++ src/App.js
@@ -1,97 +1,81 @@
-import React, { Component } from 'react';
-import './App.css';
-import { Grid, Col, Row } from 'react-bootstrap';
-import Work from './components/Work';
-import Project from './components/Project';
-import Contact from './components/Contact';
-import Self from './self.jpeg';
-import Link from 'react-router-dom';
+import React, { Component } from "react";
+import "./App.css";
+import { Grid, Col, Row } from "react-bootstrap";
+import Work from "./components/Work";
+import Project from "./components/Project";
+import Contact from "./components/Contact";
 
 class App extends Component {
-
   constructor() {
     super();
     this.state = {
-      showHome: true,
-      showWork: false,
-      showProject: false,
-      showContact: false,
-    } 
-  }
-
-  _handleHome() {
-    var show = !this.state.showHome;
-    this.setState({
-      showHome: show,
-      showWork: false,
-      showProject: false,
-      showContact: false
-    });
-  }
-
-  _handleWork() {
-      var show = !this.state.showWork;
-      this.setState({
-       showWork: show,
-       showHome: false,
-       showProject: false,
-       showContact: false
-      });
-  }
-
-  _handleProject() {
-     var show = !this.state.showProject;
-      this.setState({
-       showWork: false,
-       showHome: false,
-       showProject: show,
-       showContact: false
-     });
-  }
-
-  _handleContact() {
-    var show = !this.state.showContact;
-      this.setState({
-       showWork: false,
-       showHome: false,
-       showProject: false,
-       showContact: show
-    });
+      show: "Home",
+    };
   }
 
   render() {
+    const Component = this.state.show;
 
     return (
-      <div className="App"> 
-        <link href='https://fonts.googleapis.com/css?family=Noto+Serif|Inconsolata:400,700|Bitter'
-            rel='stylesheet'
-            type='text/css' />
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css"></link>
-        <h1 className="name" onClick={this._handleHome.bind(this)}>Valentina Dibs</h1>
+      <div className="App">
+        <link
+          href="https://fonts.googleapis.com/css?family=Noto+Serif|Inconsolata:400,700|Bitter"
+          rel="stylesheet"
+          type="text/css"
+        />
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css"
+        />
+        <h1 className="name" onClick={() => this.setState({ show: "Home" })}>
+          Valentina Dibs
+        </h1>
         <hr />
-        <Grid><Row className="show-grid">
-        <Col className="col" sm={4} md={4} xs={12} lg={4} className="show-grid">
-          <div className="intro">  
-           
-            <h2 className="bio">Bio</h2>
-            <p>Hi, I&rsquo;m Valentina Dibs - a 3rd year at UC San Diego where I study Computer Science and Cognitive Science with a specialization in HCI.</p>
-            <p>I love to create (code, sing, knit, sew), listen to podcasts (NPR politics + Ted Radio Hour), read (just started The Jungle), and exercise (started weight-lifting last year).</p> 
-          </div>
-          <div className="toolbar">
-            <a href="#" className="linkTo" onClick={this._handleWork.bind(this)}>Work</a>
-            <a href="#" className="linkTo" onClick={this._handleProject.bind(this)}>Projects</a>
-            <a href="#" className="linkTo" onClick={this._handleContact.bind(this)}>Contact</a>
-          </div>
-        </Col>
-        <Col className="col" sm={8} md={8} xs={12} lg={8} className="show-grid">
-          {this.state.showWork ?
-            <Work />  : null  } 
-          {this.state.showProject ?
-            <Project />  : null  } 
-          {this.state.showContact ?
-            <Contact />  : null  } 
-        </Col>
-        </Row></Grid>
+        <Grid>
+          <Row className="show-grid">
+            <Col className="col show-grid" sm={4} md={4} xs={12} lg={4}>
+              <div className="intro">
+                <h2 className="bio">Bio</h2>
+                <p>
+                  Hi, I&rsquo;m Valentina Dibs - a 3rd year at UC San Diego
+                  where I study Computer Science and Cognitive Science with a
+                  specialization in HCI.
+                </p>
+                <p>
+                  I love to create (code, sing, knit, sew), listen to podcasts
+                  (NPR politics + Ted Radio Hour), read (just started The
+                  Jungle), and exercise (started weight-lifting last year).
+                </p>
+              </div>
+              <div className="toolbar">
+                <a
+                  href="#"
+                  className="linkTo"
+                  onClick={() => this.setState({ show: Work })}
+                >
+                  Work
+                </a>
+                <a
+                  href="#"
+                  className="linkTo"
+                  onClick={() => this.setState({ show: Project })}
+                >
+                  Projects
+                </a>
+                <a
+                  href="#"
+                  className="linkTo"
+                  onClick={() => this.setState({ show: Contact })}
+                >
+                  Contact
+                </a>
+              </div>
+            </Col>
+            <Col className="col show-grid" sm={8} md={8} xs={12} lg={8}>
+              {Component === "Home" ? null : <Component />}
+            </Col>
+          </Row>
+        </Grid>
       </div>
     );
   }
--- src/components/ProjectObject.js
+++ src/components/ProjectObject.js
@@ -1,35 +1,42 @@
-import React, {Component} from 'react'; 
+import React, { Component } from "react";
 
 class ProjectObject extends Component {
-	constructor() {
-		super(); 
-	}
- 
+  render() {
+    return (
+      <div>
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css"
+        />
 
-	render() {
-
-		return (
-			<div>
-			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css"></link>
-			
-			<div className="timeline"></div> 
-			<div className="shadow">
-				<div className="work"> 
-			    	<div className="quarter">
-						<p>{this.props.time}</p>
-					</div> 
-					<div className="workBody">
-						<p className="company">{this.props.company}</p> 
-						<p className="description">{this.props.description}</p> 
-						{this.props.skill.map(function(skill, index) {
-							return <p className="skill">{skill}</p>
-						})}
-			    	</div>  
-				</div> 
-			</div>
-			</div>
-		);
-	}
-} 
+        <div className="timeline" />
+        <div className="shadow">
+          <div className="work">
+            <div className="quarter">
+              <p>
+                {this.props.time}
+              </p>
+            </div>
+            <div className="workBody">
+              <p className="company">
+                {this.props.company}
+              </p>
+              <p className="description">
+                {this.props.description}
+              </p>
+              {this.props.skill.map(function(skill, index) {
+                return (
+                  <p className="skill">
+                    {skill}
+                  </p>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default ProjectObject;
